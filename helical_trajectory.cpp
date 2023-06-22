abd73224064ddf9d3e8ff7daff711d673f0e553a
Stefano <stefano2.colli@mail.polimi.it>
2023-06-22 20:05:51 +0200

    Refactoring:
    - doxygen documentation
    - set default Mavlink system URL
    - reorder function arguments for consistency

--- helical_trajectory.cpp
+++ helical_trajectory.cpp
@@ -21,31 +21,31 @@
 using namespace mavsdk;
 using std::this_thread::sleep_for;
 
-// HELIX PARAMETERS
-float helix_origin[3] = {0.0f, 0.0f, -5.0f};
-float helix_radius = 5.0f;
-float helix_start[3] = {helix_radius, 0.0f, -5.0f};
-float vertical_separation = 1.0f;
-float trajectory_speed = 0.4f;
-uint64_t unix_epoch_time_us;
-uint32_t boot_time_ms;
-bool logging = true;
+// helical trajectory parameters
+float helix_origin[3] = {0.0f, 0.0f, -5.0f};            //!< origin of the helical path (NED frame)
+float helix_radius = 5.0f;                              //!< radius of the helical trajectory [m]
+float helix_start[3] = {helix_radius, 0.0f, -5.0f};     //!< starting point of the helical trajectory
+float vertical_separation = 1.0f;                       //!< vertical separation between loops in the helix [m]
+float trajectory_speed = 0.4f;                          //!< speed of the trajectory
 
-// store messages
+// global variables
+uint64_t unix_epoch_time_us;                            //!< onboard unix time [us]
+
+// ground station settings 
+bool logging = true;                                    //!< start/stop logging the position and position setpoint messages
+
+// message queues for logging
 std::queue<mavlink_position_target_local_ned_t> pos_stp_queue;
 std::queue<mavlink_local_position_ned_t> pos_queue;           
 
-void usage(const std::string &bin_name)
-{
-    std::cerr << "Usage : " << bin_name << " <connection_url>\n"
-              << "Connection URL format should be :\n"
-              << " For TCP : tcp://[server_host][:server_port]\n"
-              << " For UDP : udp://[bind_host][:bind_port]\n"
-              << " For Serial : serial:///path/to/serial/dev[:baudrate]\n"
-              << "For example, to connect to the simulator use URL: udp://:14540\n";
-}
-
-auto connect_to_system(Mavsdk &mavsdk, char *url)
+/**
+ * @brief Search and connect to Mavlink system
+ * 
+ * @param mavsdk MAVSDK instance
+ * @param url connection URL string 
+ * @return auto Mavlink system connected to
+ */
+auto connect_to_system(Mavsdk &mavsdk, const std::string& url)
 {
     ConnectionResult connection_result = mavsdk.add_any_connection(url);
 
@@ -71,12 +71,17 @@ auto connect_to_system(Mavsdk &mavsdk, char *url)
     auto system = mavsdk.systems()[0];
     if (!system)
     {
-        std::cerr << "Timed out waiting for system\n";
+        std::cerr << "Timed out waiting for system" << std::endl;
         exit(1);
     }
     return system;
 }
 
+/**
+ * @brief Check if all calibrations are done and if the drone is ready to be armed
+ * 
+ * @param telemetry Telemetry plugin instance
+ */
 void preflight_check(Telemetry &telemetry)
 {
     // Exit if calibration is required
@@ -123,6 +128,12 @@ void preflight_check(Telemetry &telemetry)
     }
 }
 
+/**
+ * @brief Arm the drone
+ * 
+ * @param action Action plugin instance
+ * @param telemetry Telemetry plugin instance
+ */
 void arm(Action &action, Telemetry &telemetry)
 {
     // Arm vehicle
@@ -141,7 +152,14 @@ void arm(Action &action, Telemetry &telemetry)
     }
 }
 
-void takeoff(Action &action, float height, Telemetry &telemetry)
+/**
+ * @brief Send takeoff command and wait for the drone to reach the desired altitude
+ * 
+ * @param height desired takeoff altitude
+ * @param action Action plugin instance
+ * @param telemetry Telemetry plugin instance
+ */
+void takeoff(float height, Action &action, Telemetry &telemetry)
 {
     action.set_takeoff_altitude(height);
 
@@ -165,6 +183,12 @@ void takeoff(Action &action, float height, Telemetry &telemetry)
     std::cout << "Takeoff Completed" << std::endl;
 }
 
+/**
+ * @brief Send land command and wait touchdown and automatic disarm
+ * 
+ * @param action Action plugin instance
+ * @param telemetry Telemetry plugin instance
+ */
 void land_and_disarm(Action &action, Telemetry &telemetry)
 {
     const Action::Result land_result = action.land();
@@ -190,6 +214,11 @@ void land_and_disarm(Action &action, Telemetry &telemetry)
     std::cout << "Drone Disarmed" << std::endl;
 }
 
+/**
+ * @brief Switch drone flight mode to 'Offboard' (https://docs.px4.io/main/en/flight_modes/offboard.html)
+ * 
+ * @param offboard Offboard plugin instance
+ */
 void start_offboard_mode(Offboard &offboard)
 {
     // Create a setpoint before starting offboard mode (in this case a null setpoint)
@@ -199,14 +228,19 @@ void start_offboard_mode(Offboard &offboard)
     Offboard::Result offboard_result = offboard.start();
     if (offboard_result != Offboard::Result::Success)
     {
-        std::cerr << "Cannot start offboard mode: " << offboard_result << std::endl;
+        std::cerr << "Cannot start Offboard mode: " << offboard_result << std::endl;
     }
     else
     {
-        std::cout << "Switched to offboard mode" << std::endl;
+        std::cout << "Switched to Offboard mode" << std::endl;
     }
 }
 
+/**
+ * @brief Stop 'Offboard' flight mode, the drone is put in 'Hold' mode (https://docs.px4.io/main/en/flight_modes/hold.html)
+ * 
+ * @param offboard Offboard plugin instance 
+ */
 void stop_offboard_mode(Offboard &offboard)
 {
     // Stop offboard mode
@@ -217,10 +251,19 @@ void stop_offboard_mode(Offboard &offboard)
     }
     else
     {
-        std::cout << "Stopped offboard mode" << std::endl;
+        std::cout << "Stopped Offboard mode, drone is in Hold mode" << std::endl;
     }
 }
 
+/**
+ * @brief Test wether drone position is in the neighborhood of the target position 
+ * 
+ * @param target_pos desired position
+ * @param acceptance_radius_m neighborhood radius [m]
+ * @param telemetry Telemetry plugin instance
+ * @return true if the drone is in the neighborhood of the target position
+ * @return false otherwise
+ */
 bool estimated_position_close_to(const Offboard::PositionNedYaw &target_pos, float acceptance_radius_m, Telemetry &telemetry)
 {
     Telemetry::PositionNed est_pos = telemetry.position_velocity_ned().position;
@@ -230,6 +273,13 @@ bool estimated_position_close_to(const Offboard::PositionNedYaw &target_pos, flo
     return distance_m < acceptance_radius_m;
 }
 
+/**
+ * @brief Steer drone to target position and wait until it is in the neighborhood of the destination
+ *  
+ * @param target_pos desired target position
+ * @param offboard Offboard plugin instance
+ * @param telemetry Telemetry plugin instance
+ */
 void offboard_goto(const Offboard::PositionNedYaw &target_pos, Offboard &offboard, Telemetry &telemetry)
 {
     // TODO approach with a certain velocity
@@ -246,6 +296,13 @@ void offboard_goto(const Offboard::PositionNedYaw &target_pos, Offboard &offboar
     std::cout << "Target reached" << std::endl;
 }
 
+/**
+ * @brief Compute next position and velocity setpoints of the helical trajectory
+ * 
+ * @param[out] target_pos output position setpoint
+ * @param[out] target_vel output velocity setpoint
+ * @param trajectory_time time elapsed from the start of the helical motion [s]
+ */
 void compute_helix_setpoint(Offboard::PositionNedYaw &target_pos, Offboard::VelocityNedYaw &target_vel, double trajectory_time)
 {
     target_pos.north_m = helix_origin[0] + helix_radius * cosf(trajectory_time * trajectory_speed);
@@ -257,7 +314,15 @@ void compute_helix_setpoint(Offboard::PositionNedYaw &target_pos, Offboard::Velo
     target_vel.down_m_s = -vertical_separation * trajectory_speed; // NED frame
 }
 
-void download_ulg(LogFiles &logfiles, std::string dest_path)
+/**
+ * @brief Download the log file (.ulg) from the drone and delete it from the onboard memory afterwards 
+ * (assuming there exists a single log file on the drone. If there are more files, the function does nothing and the 
+ * desired operation has to be carried out manually)
+ * 
+ * @param dest_path path where to store the log file 
+ * @param logfiles LogFiles plugin instance
+ */
+void download_ulg(std::string dest_path ,LogFiles &logfiles)
 {
     std::cout << "Downloading log file..." << std::endl;
     auto query_log = logfiles.get_entries();
@@ -299,7 +364,45 @@ void download_ulg(LogFiles &logfiles, std::string dest_path)
     }
 }
 
-mavsdk::MavlinkPassthrough::Result set_message_rate(MavlinkPassthrough &mavlinkpassthrough, uint message_id, float frequency)
+/**
+ * @brief Saves data logged by the control station to output file in csv format
+ * 
+ * @param filename name of the output .csv file (default is "out.csv")
+ */
+void log_to_csv(std::string filename = "out.csv")
+{
+    std::ofstream csv_file;
+    csv_file.open(filename);
+    csv_file << "pos_t,pos_x,pos_y,pos_z,stp_t,stp_x,stp_y,stp_z\n";
+
+    int shortest_queue = pos_queue.size() < pos_stp_queue.size()? pos_queue.size() : pos_stp_queue.size(); 
+    while (shortest_queue)
+    {
+        mavlink_local_position_ned_t& pos = pos_queue.front();
+        mavlink_position_target_local_ned_t& stp = pos_stp_queue.front();
+
+        csv_file << pos.time_boot_ms << "," << pos.x << "," << pos.y << "," << pos.z << ","
+                 << stp.time_boot_ms << "," << stp.x << "," << stp.y << "," << stp.z << "\n";
+
+        pos_queue.pop();
+        pos_stp_queue.pop();
+        shortest_queue --;
+    }
+    csv_file.close();
+
+    std::cout << "Saved log data to " << filename << std::endl;
+
+}
+
+/**
+ * @brief Set the message rate of a particular topic to the desired frequency 
+ * (the Offboard default link sends messages at low frequency )
+ * 
+ * @param message_id ID of the Mavlink message topic we want to set the frequency (https://mavlink.io/en/messages/common.html)
+ * @param frequency desired frequency [Hz]
+ * @param mavlinkpassthrough MavlinkPassthrough plugin instance
+ */
+void set_message_rate(uint message_id, float frequency, MavlinkPassthrough &mavlinkpassthrough)
 {
     float interval = 1/frequency *1e6f; //interval needs to be expressed in us
 
@@ -315,63 +418,92 @@ mavsdk::MavlinkPassthrough::Result set_message_rate(MavlinkPassthrough &mavlinkp
     cmd.param6 = NAN;
     cmd.param7 = (float) 0; //response target
 
-    return mavlinkpassthrough.send_command_long(cmd);
+    mavsdk::MavlinkPassthrough::Result res = mavlinkpassthrough.send_command_long(cmd);
+    if (res != MavlinkPassthrough::Result::Success)
+    {
+        std::cerr << "Can't set the desired rate of " << frequency 
+                  << " Hz, for message topic number " << message_id << std::endl;
+    }
+    
 } 
 
+/**
+ * @brief Callback of the unix epoch time updates subscriber, it keeps the control station in synch with the drone updating global variable unix_epoch_time_us
+ * 
+ * @param unix_time Unix time update from the drone
+ */
 void time_callback(uint64_t unix_time)
 {
     unix_epoch_time_us = unix_time;
 }
 
+/**
+ * @brief Callback of the POSITION_TARGET_LOCAL_NED topic subscriber (https://mavlink.io/en/messages/common.html#POSITION_TARGET_LOCAL_NED), it pushes
+ * all received messages to the log queue
+ * 
+ * @param msg incoming Mavlink message
+ */
 void setpoint_callback(const mavlink_message_t &msg)
 {
     mavlink_position_target_local_ned_t stp;
     mavlink_msg_position_target_local_ned_decode(&msg, &stp);
     pos_stp_queue.push(stp);
-    // printf("Setpoint %f , %f , %f\n", stp.x, stp.y, stp.z);
 }
 
+/**
+ * @brief Callback of the LOCAL_POSITION_NED topic subscriber (https://mavlink.io/en/messages/common.html#LOCAL_POSITION_NED), it pushes
+ * all received messages to the log queue
+ * 
+ * @param msg incoming Mavlink message
+ */
 void position_callback(const mavlink_message_t &msg)
 {
     mavlink_local_position_ned_t pos;
     mavlink_msg_local_position_ned_decode(&msg, &pos);
     pos_queue.push(pos);
-    // printf("Position %f , %f , %f\n", pos.x, pos.y, pos.z);
 }
 
-void systime_callback(const mavlink_message_t &msg)
+/**
+ * @brief Set requested topic rate to 50Hz, subscribe to requested topic and keep logging all received messages until logging variable is false 
+ * (this function is meant to be run on a separate thread for each topic we want to log)
+ * 
+ * @param message_id ID of the Mavlink message topic we want to subscribe to and log (https://mavlink.io/en/messages/common.html) 
+ * @param callback callback function to process each incoming message
+ * @param mavlinkpassthrough MavlinkPassthrough plugin instance
+ */
+void subscribe_function(uint message_id, std::function<void (const mavlink_message_t &)> callback, MavlinkPassthrough &mavlinkpassthrough)
 {
-    mavlink_system_time_t time;
-    mavlink_msg_system_time_decode(&msg, &time);
-    boot_time_ms = time.time_boot_ms;
-}
-
-void subscribe_function(MavlinkPassthrough &mavlinkpassthrough, uint mavlink_topic, std::function<void (const mavlink_message_t &)> callback)
-{
-    //subscribe requested topic at 50Hz
-    mavlinkpassthrough.subscribe_message_async(mavlink_topic, callback);
-    set_message_rate(mavlinkpassthrough, mavlink_topic, 50.f);   
+    // subscribe to requested topic at 50Hz
+    mavlinkpassthrough.subscribe_message_async(message_id, callback);
+    set_message_rate(message_id, 50.f, mavlinkpassthrough);   
 
     while (logging)
     {
     }
 
     // unsubscribe from topics
-    mavlinkpassthrough.subscribe_message_async(mavlink_topic, nullptr);
+    mavlinkpassthrough.subscribe_message_async(message_id, nullptr);
 }
 
 int main(int argc, char **argv)
 {
+    std::string system_url;
+
     if (argc != 2)
     {
-        usage(argv[0]);
-        exit(1);
+        std::cerr << "No connection URL specified, using udp://:14540\n"
+                  << "For more options visit https://mavsdk.mavlink.io/main/en/cpp/guide/connections.html" << std::endl;
+        system_url = "udp://:14540";
+    }
+    else
+    {
+        system_url = argv[1];
     }
 
     Mavsdk mavsdk;
 
     mavsdk::log::subscribe(nullptr); // avoid mavsdk print debug messages to console
-    auto system = connect_to_system(mavsdk, argv[1]);
+    auto system = connect_to_system(mavsdk, system_url);
 
     // Instantiate plugins
     Telemetry telemetry = Telemetry{system};
@@ -380,83 +512,73 @@ int main(int argc, char **argv)
     LogFiles logfiles = LogFiles{system};
     MavlinkPassthrough mavlinkpassthrough = MavlinkPassthrough{system}; // to access the undelying mavlink protocol
 
-    // Subscriptions
-    telemetry.set_rate_unix_epoch_time(50.0); // TODO increase hz or use Telemetry::unix_epoch_time();
+    // Time subscription
+    telemetry.set_rate_unix_epoch_time(50.0);
     telemetry.subscribe_unix_epoch_time(time_callback);
 
     // subscribe to mavlink topics
     // mavlinkpassthrough.subscribe_message_async(MAVLINK_MSG_ID_POSITION_TARGET_LOCAL_NED, setpoint_callback);
     // mavlinkpassthrough.subscribe_message_async(MAVLINK_MSG_ID_LOCAL_POSITION_NED, position_callback);
-    // mavlinkpassthrough.subscribe_message_async(MAVLINK_MSG_ID_SYSTEM_TIME, systime_callback);
 
-    ///// FLIGHT /////
+
+    /////////////////////////////////////////////////
+    /// FLIGHT
+    /////////////////////////////////////////////////
     preflight_check(telemetry);
     // Arm vehicle
     arm(action, telemetry);
     // Take off
-    takeoff(action, 5.0, telemetry);
+    takeoff(5.0, action, telemetry);
 
     // helical trajectory
     start_offboard_mode(offboard);
 
     // reach starting position
+    std::cout << "Approaching starting position" << std::endl;
     Offboard::PositionNedYaw starting_pos{helix_start[0], helix_start[1], helix_start[2], 0.0f};
     offboard_goto(starting_pos, offboard, telemetry);
 
-    uint64_t start_unix_time = unix_epoch_time_us;
-    // uint32_t start_time_ms = boot_time_ms;
+    uint64_t start_unix_time_us = unix_epoch_time_us;
     Offboard::PositionNedYaw pos_stp;
     Offboard::VelocityNedYaw vel_stp;
 
     // start threads to log incoming messages
-    std::thread thread_pos(subscribe_function, std::ref(mavlinkpassthrough), MAVLINK_MSG_ID_LOCAL_POSITION_NED, position_callback);
-    std::thread thread_pos_stp(subscribe_function, std::ref(mavlinkpassthrough), MAVLINK_MSG_ID_POSITION_TARGET_LOCAL_NED, setpoint_callback);
+    std::thread thread_pos(subscribe_function, MAVLINK_MSG_ID_LOCAL_POSITION_NED, position_callback, std::ref(mavlinkpassthrough));
+    std::thread thread_pos_stp(subscribe_function, MAVLINK_MSG_ID_POSITION_TARGET_LOCAL_NED, setpoint_callback, std::ref(mavlinkpassthrough));
 
+    std::cout << "Performing helical trajectory" << std::endl;
     double trajectory_time = 0.0;
     while (trajectory_time < 30.0)
     {
-        trajectory_time = (unix_epoch_time_us - start_unix_time) / 1e6;
-        // trajectory_time = (boot_time_ms - start_time_ms) / 1e3;
+        trajectory_time = (unix_epoch_time_us - start_unix_time_us) / 1e6;
 
         compute_helix_setpoint(pos_stp, vel_stp, trajectory_time);
         offboard.set_position_velocity_ned(pos_stp, vel_stp);
-        sleep_for(std::chrono::milliseconds(20)); // send setpoints @50Hz
 
-        //offboard.set_position_ned(pos_stp); //TODO what is the difference wrt using set_position_velocity_ned?
+        sleep_for(std::chrono::milliseconds(20)); // send setpoints at 50Hz
     }
     stop_offboard_mode(offboard);
     logging = false;
 
     land_and_disarm(action, telemetry);
 
-    ///// POST FLIGHT /////
+    //disconnect from drone and free resources
+    mavsdk.~Mavsdk();
+
+
+    /////////////////////////////////////////////////
+    /// POST FLIGHT
+    /////////////////////////////////////////////////
     
-    //join logging threads
+    // join logging threads
     thread_pos.join();
     thread_pos_stp.join();
 
-    download_ulg(logfiles, "./");
+    // if needed we can download the entire log file from the drone
+    //download_ulg("./", logfiles);
 
-    //save logs to csv file
-    std::ofstream csv_file;
-    csv_file.open ("out.csv");
-    csv_file << "pos_t,pos_x,pos_y,pos_z,stp_t,stp_x,stp_y,stp_z\n";
-
-    int shortest_queue = pos_queue.size() < pos_stp_queue.size()? pos_queue.size() : pos_stp_queue.size(); 
-    //TODO save 2 files and merge with pandas?
-    while (shortest_queue)
-    {
-        mavlink_local_position_ned_t& pos = pos_queue.front();
-        mavlink_position_target_local_ned_t& stp = pos_stp_queue.front();
-
-        csv_file << pos.time_boot_ms << "," << pos.x << "," << pos.y << "," << pos.z << ","
-                 << stp.time_boot_ms << "," << stp.x << "," << stp.y << "," << stp.z << "\n";
-
-        pos_queue.pop();
-        pos_stp_queue.pop();
-        shortest_queue --;
-    }
-    csv_file.close();
+    // save logs to csv file
+    log_to_csv();
 
     return 0;
 }
